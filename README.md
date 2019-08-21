1a05d829de99cba1af001c9844a2c4f23ef4f6f7
Anders Demant van der Weide <antra@antra.dk>
2019-08-21 14:22:28 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MyFirstScrape
-Quick stab at a web scraper
+Quick stab at a web scraper against MTA turnstile data:
+http://web.mta.info/developers/turnstile.html
+
+Source:
+https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
---
+++ app.py
@@ -0,0 +1,24 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+url = 'http://web.mta.info/developers/turnstile.html'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+soup.findAll('a')
+
+one_a_tag = soup.findAll('a')[36]
+link = one_a_tag['href']
+download_url = 'http://web.mta.info/developers/' + link
+#urllib.request.urlretrieve(download_url, './'+link[link.find('turnstile_')+1:])
+
+
+for i in range(36, len(soup.findAll('a')) + 1):
+    one_a_tag = soup.findAll('a')[i]
+    link = one_a_tag['href']
+    download_url = 'http://web.mta.info/developers/' + link
+    urllib.request.urlretrieve(
+        download_url, './'+link[link.find('/turnstile_')+1:])
+    time.sleep(1)
